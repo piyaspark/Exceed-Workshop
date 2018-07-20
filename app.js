273553a70119ccfa980ee3ae2773af4f0a90c7a7
Piyaphol Wiengperm <piyaphol.w@ku.th>
2018-07-20 15:49:41 +0700

    update

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
+
 $(function () {
-    // $('#add-button').on('click', function () {
-    //     if ($('#input1').val() === "" || $('#input2').val() === "") {
-    //         alert("กรอกกกกก")
-    //     } else {
-    //         $('#box').append(`
-    //        <div>
-    //         <h3>${$("#input1").val()}</h3>
-    //         <p>${$("#input2").val()}</p>
-    //         <hr>
-    //         </div>
-    //        `)
-    //         $('#input1').val("")
-    //         $('#input2').val("")
-    //     }
-    // })
-    
-    let old = ""
     setInterval(function () {
         $.ajax({
             type: "GET",
-            url: "http://exceed.srakrn.me/api/jacky-group/view/",
+            url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-buzzer/view/",
             dataType: "text",
             success: function (response) {
-                if(old !== response){
-                    old = response
-                    $('#box').append(`<h4 class="text-left">Guess</h4>`)
-                    $(`#box`).append(`
-                    <h3 class="alert alert-primary" role="alert">${response}</h3>
-                    `)
-                    
-                }
+                
+            },
+            fail: function (response) {
+                console.log(response)
+            }
+        });
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-door/view/",
+            dataType: "text",
+            success: function (response) {
+                
+            },
+            fail: function (response) {
+                console.log(response)
+            }
+        });
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-light/view/",
+            dataType: "text",
+            success: function (response) {
+                
+            },
+            fail: function (response) {
+                console.log(response)
+            }
+        });
+
+        $.ajax({
+            type: "GET",
+            url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-temp/view/",
+            dataType: "text",
+            success: function (response) {
+                
             },
             fail: function (response) {
                 console.log(response)
@@ -37,29 +50,97 @@ $(function () {
         });
     }, 1000)
 
-    $('#send-button').on('click', function () {
 
-        $.ajax({
-            type: "POST",
-            url: "http://exceed.srakrn.me/api/jacky-group/set/",
-            data: {
-                value: $('#text-input').val()
-            },
-            dataType: "json",
-            success: function (response) {
-            if(old !== response)
-                old = response
-                $('#box').append(`<h4 class="text-right">You</h4>`)
-                $('#box').append(`<h3 class="text-right alert alert-success" role="alert">${$("#text-input").val()}</h3>`).val()
-                console.log(response)
-
-            }
-        });
+    //Buzzer Post
+    $(function () {
+        var val;
+        $('#buzzer-toggle').on('change', function () {
+            let check = $('#buzzer-toggle').prop('checked')
+            if(check)val = "1"
+            else val = "0"
+            $.ajax({
+                type: "POST",
+                url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-buzzer/set/",
+                data: { value: val }
+                ,
+                dataType: "JSON",
+                success: function (response) {
+                    console.log(response)
+                },
+                fail: function (response) {
+                    console.log(response)
+                }
+            });
+        })
     })
 
 
+    //LED Post
+    $(function () { 
+        var val;
+        $('#led-toggle').on('change', function () {
+            let check = $('#led-toggle').prop('checked')
+            if(check)val = "1"
+            else val = "0"
+            $.ajax({
+                type: "POST",
+                url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-led/set/",
+                data: { value: val }
+                ,
+                dataType: "JSON",
+                success: function (response) {
+                    console.log(response)
+                },
+                fail: function (response) {
+                    console.log(response)
+                }
+            });
+        })
+    })
 
 
+    //Air Post
+    $(function () {
+        $('#air-toggle').on('change', function () {
+            let check = $('#air-toggle').prop('checked')
+            if(check)val = "1"
+            else val = "0"
+            $.ajax({
+                type: "POST",
+                url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-air/set/",
+                data: { value: val }
+                ,
+                dataType: "JSON",
+                success: function (response) {
+                    console.log(response)
+                },
+                fail: function (response) {
+                    console.log(response)
+                }
+            });
+        })
+    })
 
+    //Door Post
+    $(function () {
+        $('#door-toggle').on('change', function () {
+            let check = $('#door-toggle').prop('checked')
+            if(check)val = "1"
+            else val = "0"
+            $.ajax({
+                type: "POST",
+                url: "http://ecourse.cpe.ku.ac.th:1515/api/pf-door/set/",
+                data: { value: val }
+                ,
+                dataType: "JSON",
+                success: function (response) {
+                    console.log(response)
+                },
+                fail: function (response) {
+                    console.log(response)
+                }
+            });
+        })
+})
 
 })
